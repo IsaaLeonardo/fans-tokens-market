1b5783d6caf1db665044a1ba18d9f97e00137630
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-02-28 22:05:15 -0400

    Format result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,14 +1,37 @@
+import styled from "@emotion/styled"
+
+const Container = styled.div`
+  color: #FFF;
+  font-family: 'Lato', sans-serif;
+`
+
+const ParagraphPrice = styled.p`
+  font-size: 30px;
+
+  span {
+    font-weight: 700;
+  }
+`
+
+const ParagraphText = styled.p`
+  font-size: 18px;
+
+  span {
+    font-weight: 700;
+  }
+`
+
 function Result({ result }) {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = result
 
   return (
-    <div>
-      <p>El precio es de: <span>{ PRICE }</span></p>
-      <p>El precio más alto del día: <span>{ HIGHDAY }</span></p>
-      <p>El precio más bajo del día: <span>{ LOWDAY }</span></p>
-      <p>Variación últimas 24 horas: <span>{ CHANGEPCT24HOUR }</span></p>
-      <p>Última actualización: <span>{ LASTUPDATE }</span></p>
-    </div>
+    <Container>
+      <ParagraphPrice>El precio es de: <span>{ PRICE }</span></ParagraphPrice>
+      <ParagraphText>El precio más alto del día: <span>{ HIGHDAY }</span></ParagraphText>
+      <ParagraphText>El precio más bajo del día: <span>{ LOWDAY }</span></ParagraphText>
+      <ParagraphText>Variación últimas 24 horas: <span>{ CHANGEPCT24HOUR }</span></ParagraphText>
+      <ParagraphText>Última actualización: <span>{ LASTUPDATE }</span></ParagraphText>
+    </Container>
   )
 }
 
