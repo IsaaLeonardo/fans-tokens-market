fbb8d6dc6a5a6c13605807af3e280245c734b026
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-02-28 22:19:23 -0400

    Add image to show result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,6 +3,11 @@ import styled from "@emotion/styled"
 const Container = styled.div`
   color: #FFF;
   font-family: 'Lato', sans-serif;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 50px;
 `
 
 const ParagraphPrice = styled.p`
@@ -21,16 +26,32 @@ const ParagraphText = styled.p`
   }
 `
 
+const ImageContainer = styled.div`
+  width: 120px;
+
+  img {
+    max-width: 100%;
+  }
+`
+
+const InfoContainer = styled.div`
+`
+
 function Result({ result }) {
-  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = result
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE, IMAGEURL } = result
 
   return (
     <Container>
-      <ParagraphPrice>El precio es de: <span>{ PRICE }</span></ParagraphPrice>
-      <ParagraphText>El precio más alto del día: <span>{ HIGHDAY }</span></ParagraphText>
-      <ParagraphText>El precio más bajo del día: <span>{ LOWDAY }</span></ParagraphText>
-      <ParagraphText>Variación últimas 24 horas: <span>{ CHANGEPCT24HOUR }</span></ParagraphText>
-      <ParagraphText>Última actualización: <span>{ LASTUPDATE }</span></ParagraphText>
+      <ImageContainer>
+        <img src={`https://cryptocompare.com/${IMAGEURL}`} alt="crypto logo"/>
+      </ImageContainer>
+      <InfoContainer>
+        <ParagraphPrice>El precio es de: <span>{ PRICE }</span></ParagraphPrice>
+        <ParagraphText>El precio más alto del día: <span>{ HIGHDAY }</span></ParagraphText>
+        <ParagraphText>El precio más bajo del día: <span>{ LOWDAY }</span></ParagraphText>
+        <ParagraphText>Variación últimas 24 horas: <span>{ CHANGEPCT24HOUR }</span></ParagraphText>
+        <ParagraphText>Última actualización: <span>{ LASTUPDATE }</span></ParagraphText>
+      </InfoContainer>
     </Container>
   )
 }
